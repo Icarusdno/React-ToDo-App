e7ddf74049ca89ee27c98f9bcd33ede1fc299980
Icarus <noobishero@gmail.com>
2018-12-12 17:45:22 +0500

    Updates

---
+++ precache-manifest.623e8c65dd8821f6e3dea81a8642f1a9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8b87af0de3802cc8a717",
+    "url": "/React-ToDo-App/static/js/runtime~main.8b87af0d.js"
+  },
+  {
+    "revision": "841ff5b64268d2d643cb",
+    "url": "/React-ToDo-App/static/js/main.841ff5b6.chunk.js"
+  },
+  {
+    "revision": "c26583f2bc635d6b5c6e",
+    "url": "/React-ToDo-App/static/js/1.c26583f2.chunk.js"
+  },
+  {
+    "revision": "841ff5b64268d2d643cb",
+    "url": "/React-ToDo-App/static/css/main.f809f4b9.chunk.css"
+  },
+  {
+    "revision": "753a0e81ce9b68b61d9cb47fc3a71627",
+    "url": "/React-ToDo-App/index.html"
+  }
+];
